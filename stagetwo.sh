e9f5480b3b849a0b417defe66d0761e77b8b1aec
Tamilarasu Ubuntu <tamilarasu@ubuntu.com>
2022-04-03 13:23:16 +0000

    Renamed the stagetwo files

---
+++ stagetwo.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Set the default port
+DEFAULT_PORT="22"
+
+#Get the port number as an argument
+if [[ ! -z "${1}" ]]
+then
+    DEFAULT_PORT="${1}"
+fi
+
+#Get the input from the file and ssh into the client machines and run the script
+paste -d@ domain_name.txt password.txt | while IFS="@" read -r domain password
+do
+	#	echo "sshpass -p ""${password}"" ssh -o StrictHostKeyChecking=no root@"${domain}" ""bash -s"" < internal_file_copier.sh > ""${domain}"".pub"
+		sshpass -p ""${password}"" ssh -p "${DEFAULT_PORT}" -o StrictHostKeyChecking=no root@"${domain}" "bash -s" < client_script_runner.sh >>\
+		       	"stagethree.err"
+					
+	done
---
+++ support02.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#Path of the files.
+#SCRIPT_PATH="/root/bin/backup-rsync.sh"
+#DATABASE_PATH="/root/bin/mysql-rsync.sh"
+
+#Error log path
+ERROR_PATH=`pwd`/error.log
+
+touch "${ERROR_PATH}"
+
+echo "BOTH SCRIPTS ARE RUNNING ON ${HOSTNAME}" >> "${ERROR_PATH}"
+
+HOSTNAME=$(hostname -s)
+
+HOME_FOLDER="${HOME}"
+
+SCRIPT_PATH="${HOME_FOLDER}/script.sh"
+DATABASE_PATH="${HOME_FOLDER}/mysql.sh"
+
+
+#Execute the files
+if [[ -f "${SCRIPT_PATH}" ]]
+then
+    echo "Running the Script !!!" >> "${ERROR_PATH}"
+    bash "${SCRIPT_PATH}" >> "${ERROR_PATH}"
+else
+    echo "SCRIPT files does not exist !!! " >> "${SCRIPT_PATH}"
+fi
+
+if [[ -f "${DATABASE_PATH}" ]]
+then
+    echo "Running the DATABASE Script !!!" >> "${ERROR_PATH}"
+    bash "${DATABASE_PATH}" >> "${ERROR_PATH}"
+else
+    echo "DATABASE file does not exist"
+fi
+
+echo "BOTH SCRIPTS HAVE RAN SUCCESSFULLY ON ${HOSTNAME}" >> "${ERROR_PATH}"
+echo "" >> "${ERROR_PATH}"
+cat "${ERROR_PATH}"
