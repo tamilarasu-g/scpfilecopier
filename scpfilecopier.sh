584c784a392da5dcf7fa126e747f7129f6c1f99d
Tamilarasu Ubuntu <tamilarasu@ubuntu.com>
2022-04-03 07:11:33 +0000

    SSH PORT variable added scpfilecopier.sh

--- scpfilecopier.sh
+++ scpfilecopier.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+SSH_PORT="22"
+
 #Get the input from the file.
 paste -d@ domain_name.txt password.txt | while IFS="@" read -r domain password
 do
 	#	echo "sshpass -p ""${password}"" ssh -o StrictHostKeyChecking=no root@"${domain}" ""bash -s"" < internal_file_copier.sh > ""${domain}"".pub"
 		sshpass -p ""${password}"" ssh -o StrictHostKeyChecking=no root@"${domain}" "bash -s" < internal_file_copier.sh >\
 		       	"${domain}.pub"
-					
+		#sshpass -p ""${password}"" ssh -p "${SSH_PORT}" -o StrictHostKeyChecking=no root@"${domain}" "bash -s" < internal_file_copier.sh >\
+				"${domain}.pub"		
 	done
 
 #Check if any empty files exist..
