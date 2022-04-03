d4dfc162274aaa0f0afbe2bb2fdcb3cd18223000
Tamilarasu Ubuntu <tamilarasu@ubuntu.com>
2022-04-03 09:25:11 +0000

    Added SSH_PORT as an optional argument

--- scpfilecopier.sh
+++ scpfilecopier.sh
@@ -2,14 +2,17 @@
 
 SSH_PORT="22"
 
+if [[ ! -z "${1}" ]]
+then
+		SSH_PORT="${1}"
+fi
+
 #Get the input from the file.
 paste -d@ domain_name.txt password.txt | while IFS="@" read -r domain password
 do
 	#	echo "sshpass -p ""${password}"" ssh -o StrictHostKeyChecking=no root@"${domain}" ""bash -s"" < internal_file_copier.sh > ""${domain}"".pub"
-		sshpass -p ""${password}"" ssh -o StrictHostKeyChecking=no root@"${domain}" "bash -s" < internal_file_copier.sh >\
-		       	"${domain}.pub"
-		#sshpass -p ""${password}"" ssh -p "${SSH_PORT}" -o StrictHostKeyChecking=no root@"${domain}" "bash -s" < internal_file_copier.sh >\
-		#		"${domain}.pub"		
+		sshpass -p ""${password}"" ssh -p "${SSH_PORT}" -o StrictHostKeyChecking=no root@"${domain}" "bash -s" < internal_file_copier.sh >\
+				"${domain}.pub"		
 	done
 
 #Check if any empty files exist..
