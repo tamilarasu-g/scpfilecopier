92c05050f687beb67fb09d10b4d039cc7d25fd3a
Tamilarasu Ubuntu <tamilarasu@ubuntu.com>
2022-04-03 13:25:47 +0000

    Changed name of Support Files

--- stageone.sh
+++ stageone.sh
@@ -11,7 +11,7 @@ fi
 paste -d@ domain_name.txt password.txt | while IFS="@" read -r domain password
 do
 	#	echo "sshpass -p ""${password}"" ssh -o StrictHostKeyChecking=no root@"${domain}" ""bash -s"" < internal_file_copier.sh > ""${domain}"".pub"
-		sshpass -p ""${password}"" ssh -p "${SSH_PORT}" -o StrictHostKeyChecking=no root@"${domain}" "bash -s" < internal_file_copier.sh >\
+		sshpass -p ""${password}"" ssh -p "${SSH_PORT}" -o StrictHostKeyChecking=no root@"${domain}" "bash -s" < support01.sh >\
 				"${domain}.pub"		
 	done
 
