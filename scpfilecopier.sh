5c04603ef61aca94ce2ee1ff57b65ebb32d5184b
Tamilarasu <tamilaguru08150@gmail.com>
2022-04-03 09:50:47 +0530

    Stage 1 completed

--- scpfilecopier.sh
+++ scpfilecopier.sh
@@ -8,3 +8,18 @@ do
 		       	"${domain}.pub"
 					
 	done
+
+#Check if any empty files exist..
+PWD=$(pwd)
+
+find `${pwd}` -empty -type f -delete
+
+#Move all the .pub files to the desired folder
+
+HOME_FOLDER=$HOME
+
+DESTINATION_FOLDER="$HOME_FOLDER"/pubfiles/
+
+echo "Moving the files to the desired folder"
+mv *.pub "${DESTINATION_FOLDER}"
+echo "Successfully moved the files !!!"
