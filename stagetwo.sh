3ba84b767cb83a745a1c3efc34a9d21249abeb80
Tamilarasu Alma Linux <Tamilarasu Alma Linux>
2022-04-30 01:04:28 +0530

    Migrated from TXT to CSV files

--- stagetwo.sh
+++ stagetwo.sh
@@ -10,10 +10,10 @@ then
 fi
 
 #Get the input from the file and ssh into the client machines and run the script
-paste -d@ domain_name.txt password.txt | while IFS="@" read -r domain password
+while IFS="," read -r domain password
 do
 	#	echo "sshpass -p ""${password}"" ssh -o StrictHostKeyChecking=no root@"${domain}" ""bash -s"" < internal_file_copier.sh > ""${domain}"".pub"
 		sshpass -p ""${password}"" ssh -p "${DEFAULT_PORT}" -o StrictHostKeyChecking=no root@"${domain}" "bash -s" < support02.sh >>\
 		       	"stagetwo.err"
 					
-	done
+	done < data.csv
