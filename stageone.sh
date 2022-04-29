aa0db52ded4a6bcd0e90d9bfd50e4979ed2fa200
Tamilarasu Alma Linux <Tamilarasu Alma Linux>
2022-04-30 01:02:13 +0530

    Migrated from TXT to CSV files

--- stageone.sh
+++ stageone.sh
@@ -8,12 +8,12 @@ then
 fi
 
 #Get the input from the file.
-paste -d@ domain_name.txt password.txt | while IFS="@" read -r domain password
+while IFS="," read -r domain password
 do
 	#	echo "sshpass -p ""${password}"" ssh -o StrictHostKeyChecking=no root@"${domain}" ""bash -s"" < internal_file_copier.sh > ""${domain}"".pub"
 		sshpass -p ""${password}"" ssh -p "${SSH_PORT}" -o StrictHostKeyChecking=no root@"${domain}" "bash -s" < support01.sh >\
 				"${domain}.pub"		
-	done
+	done < data.csv
 
 #Check if any empty files exist..
 PWD=$(pwd)
